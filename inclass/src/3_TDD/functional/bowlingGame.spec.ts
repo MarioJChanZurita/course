6df4ce3c911a45f3859e759aba6d70763e435309
Mario J Chan Zurita <mchan@dealergeek.com>
2024-02-07 12:18:34 -0600

    Tarea de TDD

--- inclass/src/3_TDD/functional/bowlingGame.spec.ts
+++ inclass/src/3_TDD/functional/bowlingGame.spec.ts
@@ -1,8 +1,42 @@
+import { createBowlingGame, Game } from './bowlingGame';
 
 describe('Test bowling game', () => {
-
-  it('dummy test', () => {
-    expect(1).toBe(1)
+  let game: Game
+  beforeEach(() => {
+    game = createBowlingGame()
   })
 
+  it('should roll a gutter game', () => {
+    game = rollMany(game, 20, 0);
+    expect(game.getScore()).toBe(0)
+  })
+
+  it('should roll all ones', () => {
+    game = rollMany(game, 20, 1)
+    expect(game.getScore()).toBe(20)
+  })
+
+  it('should roll a spare', () => {
+    game = game.roll(5).roll(5).roll(4)
+    game = rollMany(game, 17, 0)
+    expect(game.getScore()).toBe(18)
+  })
+
+  it('should roll a strake', () => {
+    game = game.roll(10).roll(4).roll(1)
+    game = rollMany(game, 17, 0)
+    expect(game.getScore()).toBe(20)
+  })
+
+  it('should do a perfect game', () => {
+    game = rollMany(game, 12, 10)
+    expect(game.getScore()).toBe(300)
+  })
 });
+
+function rollMany(game: Game, rolls: number, pins: number) {
+  for (let i = 0; i < rolls; i++) {
+    game = game.roll(pins);
+  }
+  return game;
+}
